38241cf5ed94623005c7239653a2e58711a6a411
Deivid Martins <159496589+Deivid-Martins@users.noreply.github.com>
2024-11-21 18:39:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const URL_API = "https://zp2zegwi15.execute-api.sa-east-1.amazonaws.com";
-
 document.getElementById("linkForm").addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    document.getElementById("url").addEventListener("blur", function() {
-        let url = this.value;
-        // Verificar se o link começa com qualquer outra coisa sem "https://"
-        if (url && !url.startsWith('http://') && !url.startsWith('https://')) {
-            this.value = 'https://' + url;
-        }
-    });
+    const originalUrlInput = document.getElementById("originalUrl");
+    let originalUrl = originalUrlInput.value.trim();
+
+    // Verifica se a URL não começa com http:// ou https:// e adiciona https://
+    if (originalUrl && !originalUrl.startsWith('http://') && !originalUrl.startsWith('https://')) {
+        originalUrl = 'https://' + originalUrl; // Corrige a URL
+        originalUrlInput.value = originalUrl;
+    }
+
     document.getElementById("button").textContent = "Wait a bit...";
 
+    // Calculando a data de expiração (14 dias depois)
     const currentDate = new Date();
     const expirationDateInMilliseconds = currentDate.getTime() + 14 * 24 * 60 * 60 * 1000;
-    //Expira 14 dias após a criação
     const expirationTimestampInSeconds = Math.floor(expirationDateInMilliseconds / 1000);
 
     try {
@@ -22,14 +22,15 @@ document.getElementById("linkForm").addEventListener("submit", async (event) => 
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                originalUrl: originalUrl, 
+                originalUrl: originalUrl,
                 expirationTime: expirationTimestampInSeconds.toString(),
             }),
         });
 
+        // Processa a resposta do servidor
         const result = await response.json();
         document.getElementById("result").textContent = `https://zp2zegwi15.execute-api.sa-east-1.amazonaws.com/${result.code}`;
-        document.getElementById("button").textContent = "Submit";
+        document.getElementById("button").textContent = "Submit"; // Restaura o texto do botão
     } catch (error) {
         console.error("Erro ao encurtar link:", error);
     }
