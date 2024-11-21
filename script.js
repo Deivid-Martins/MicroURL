92090a13b685d5251cd5d2fa1156d51e34ea313e
Deivid Martins <159496589+Deivid-Martins@users.noreply.github.com>
2024-11-21 08:39:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const URL_API = "https://zp2zegwi15.execute-api.sa-east-1.amazonaws.com";
+
 document.getElementById("linkForm").addEventListener("submit", async (event) => {
     event.preventDefault();
 
